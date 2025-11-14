b083fbfa59acbca76d6f1d0e51d7f197f70d22f8
Timur Turatbekov <timatur242005@gmail.com>
2025-11-14 09:06:15 +0600

    Migrate to websockets

--- lib/domain/socket/socket_repository.dart
+++ lib/domain/socket/socket_repository.dart
@@ -9,7 +9,7 @@ import 'package:package_flutter/domain/core/env/env.dart';
 import 'package:package_flutter/domain/inventory/inventory.dart';
 import 'package:package_flutter/domain/user/user.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
-import 'package:socket_io_client/socket_io_client.dart' as io;
+import 'package:web_socket_channel/web_socket_channel.dart';
 
 part 'socket_repository.g.dart';
 
@@ -21,100 +21,122 @@ SocketRepository socketRepository(Ref ref) {
 }
 
 class SocketRepository {
-  io.Socket? _socket;
+  WebSocketChannel? _channel;
+  StreamController<Map<String, dynamic>>? _messagesController;
 
   final AuthRepository authRepository;
 
   SocketRepository(this.authRepository);
 
   Future<void> connect() async {
-    if (_socket == null) {
-      final serverSocketUrl = Env.getServerUrl();
-
-      _socket = io.io(
-        serverSocketUrl,
-        io.OptionBuilder()
-            .setAuth({'token': await authRepository.getToken()})
-            .setTransports(
-              ['websocket'],
-            )
-            .disableAutoConnect()
-            .build(),
-      );
-
-      _socket!.on('connect', (_) {
-        Logger().d('Socket connected');
-      });
-      _socket!.on('connect_error', (e) {
-        Logger().e('Connection socket error! $e');
-      });
+    if (_channel != null) {
+      Logger().d('Socket already connected');
+      return;
     }
 
-    if (_socket!.connected) return;
+    try {
+      final token = await authRepository.getToken();
+      final serverUrl = Env.getServerUrl();
 
-    _socket!.connect();
+      // Заменяем http на ws
+      String wsUrl = serverUrl.replaceFirst('http', 'ws');
+      wsUrl = wsUrl.replaceFirst('3001', '3002');
+      Logger().d('Connecting to the $wsUrl');
+
+      _channel = WebSocketChannel.connect(
+        Uri.parse('$wsUrl?token=$token'),
+      );
+      await _channel!.ready;
+
+      _messagesController = StreamController<Map<String, dynamic>>.broadcast();
+
+      _channel!.stream.listen(
+        (message) {
+          try {
+            final data = jsonDecode(message as String) as Map<String, dynamic>;
+            Logger().d('Received message: $data');
+
+            if (data['type'] == 'connected') {
+              Logger().d('Socket connected as: ${data['uid']}');
+              return;
+            }
+
+            _messagesController?.add(data);
+          } catch (e) {
+            Logger().e('Error parsing message: $e');
+          }
+        },
+        onError: (error) {
+          Logger().e('WebSocket error: $error');
+          _reconnect();
+        },
+        onDone: () {
+          Logger().w('WebSocket closed');
+          _reconnect();
+        },
+      );
+
+      Logger().d('Socket connected');
+    } catch (e) {
+      Logger().e('Connection error: $e');
+    }
+  }
+
+  Future<void> _reconnect() async {
+    await disconnect();
+    await Future.delayed(const Duration(seconds: 3));
+    await connect();
   }
 
   Future<void> disconnect() async {
-    if (_socket == null && _socket!.disconnected) return;
-
-    _socket?.disconnect();
+    await _channel?.sink.close();
+    await _messagesController?.close();
+    _channel = null;
+    _messagesController = null;
   }
 
   Stream<User> getUserUpdates() async* {
-    if (_socket == null) {
+    if (_messagesController == null) {
       Logger().w('Socket is not connected');
       return;
     }
 
-    final userStreamController = StreamController<User>();
-
-    _socket!.on('update', (data) {
-      final rawJson = jsonDecode(data as String);
-      Logger().d('User parsed $rawJson');
-      userStreamController.add(User.fromJson(rawJson as Map<String, dynamic>));
+    yield* _messagesController!.stream
+        .where((data) => data['channel'] == 'update')
+        .map((data) {
+      Logger().d('User parsed: ${data['data']}');
+      return User.fromJson(data['data'] as Map<String, dynamic>);
     });
-    Logger().d('Updates listening');
-    yield* userStreamController.stream;
   }
 
   Stream<Inventory> getInventoryUpdates() async* {
-    if (_socket == null) {
+    if (_messagesController == null) {
       Logger().w('Socket is not connected');
       return;
     }
 
-    final inventoryStreamController = StreamController<Inventory>();
-
-    _socket!.on('update', (data) {
-      final rawJson = jsonDecode(data as String);
-      final inventory = Inventory.fromJson(rawJson as Map<String, dynamic>);
-      inventoryStreamController.add(inventory);
+    yield* _messagesController!.stream
+        .where((data) => data['channel'] == 'update')
+        .map((data) {
+      final inventory =
+          Inventory.fromJson(data['data'] as Map<String, dynamic>);
       Logger().d('Inventory parsed: $inventory');
+      return inventory;
     });
-
-    yield* inventoryStreamController.stream;
   }
 
   Stream<Ban?> getBanUpdates() async* {
-    if (_socket == null) {
+    if (_messagesController == null) {
       Logger().w('Socket is not connected');
       return;
     }
 
-    final banStreamController = StreamController<Ban?>();
-
-    _socket!.on('banUpdate', (data) {
-      Logger().d('Ban received, $data');
-      final rawJson = jsonDecode(data as String);
-      if (rawJson == null) {
-        banStreamController.add(null);
-        return;
-      }
-      final ban = Ban.fromJson(rawJson as Map<String, dynamic>);
-      banStreamController.add(ban);
+    yield* _messagesController!.stream
+        .where((data) => data['channel'] == 'banUpdate')
+        .map((data) {
+      Logger().d('Ban received: ${data['data']}');
+      if (data['data'] == null) return null;
+      return Ban.fromJson(data['data'] as Map<String, dynamic>);
     });
-
-    yield* banStreamController.stream;
   }
 }
